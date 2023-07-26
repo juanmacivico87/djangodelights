2b4b8ba8a82ba08bad436e8c2de1fb3cce3bdd5e
juanmacivico87 <juanmacivico87@gmail.com>
2023-07-26 19:01:09 +0200

    Change max length value for unit field of ingredient model

--- inventory/models/ingredient.py
+++ inventory/models/ingredient.py
@@ -3,5 +3,5 @@ from django.db import models
 class Ingredient(models.Model):
     name = models.CharField(max_length=20)
     quantity = models.PositiveIntegerField(default=0)
-    unit = models.CharField(max_length=5)
+    unit = models.CharField(max_length=10)
     price_per_unit = models.FloatField(default=0.0)
