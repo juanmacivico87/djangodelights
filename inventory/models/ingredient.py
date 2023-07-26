4e56136296ada6c41998dffa59f37b2fc09f2297
juanmacivico87 <juanmacivico87@gmail.com>
2023-07-26 19:22:15 +0200

    Change type of quantity field and change text of items of Ingredient model

--- inventory/models/ingredient.py
+++ inventory/models/ingredient.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=20)
-    quantity = models.PositiveIntegerField(default=0)
+    quantity = models.FloatField(default=0.0)
     unit = models.CharField(max_length=10)
     price_per_unit = models.FloatField(default=0.0)
     
     def __str__(self):
-        return self.name
+        return f'{self.name} ({self.quantity} {self.unit})'
