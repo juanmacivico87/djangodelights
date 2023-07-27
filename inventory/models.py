37b2cdc0b658c2a1a15dc99c2a5832d41fc9b495
juanmacivico87 <juanmacivico87@gmail.com>
2023-07-27 11:32:48 +0200

    Create Recipe Requirement model

--- inventory/models.py
+++ inventory/models.py
@@ -1,6 +1,8 @@
 from inventory.models.ingredient import Ingredient
 from inventory.models.menuItem import MenuItem
+from inventory.models.recipeRequirement import RecipeRequirement
 
 # Create your models here.
 ingredient = Ingredient()
 menu_item = MenuItem()
+recipe_requirement = RecipeRequirement()
---
+++ inventory/models/recipeRequirement.py
@@ -0,0 +1,16 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+from inventory.models.ingredient import Ingredient
+from inventory.models.menuItem import MenuItem
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0.0)
+    
+    def __str__(self):
+        return f'{self.menu_item.title} - {self.ingredient.name}'
+    
+    def clean(self):
+        if self.quantity < 0:
+            raise ValidationError('You cannot have a recipe requirement with a quantity less than zero.')
