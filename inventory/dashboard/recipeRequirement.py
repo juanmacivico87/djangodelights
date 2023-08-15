625bc3e3d8982fd67a9f5f1b2bad257e9c4744d4
juanmacivico87 <juanmacivico87@gmail.com>
2023-08-15 18:28:40 +0200

    Add quantity column for Recipe Requeriment list in admin dashboard

--- inventory/dashboard/recipeRequirement.py
+++ inventory/dashboard/recipeRequirement.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 class RecipeRequirementAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'ingredient', 'menu_item']
+    list_display = ['__str__', 'ingredient', 'menu_item', 'quantity']
     list_filter = [
         ('ingredient', admin.RelatedOnlyFieldListFilter),
         ('menu_item', admin.RelatedOnlyFieldListFilter),
