0d6e7b5a6720b0ce0b5decd81ee80b990defe14f
juanmacivico87 <juanmacivico87@gmail.com>
2023-09-23 14:12:23 +0200

    Generate random secret key instead get it from environment variable

--- djangodelights/settings.py
+++ djangodelights/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from os import getenv
 from dotenv import load_dotenv
+from django.core.management.utils import get_random_secret_key
 
 load_dotenv()
 
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv('SECRET_KEY')
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = eval(getenv('DEBUG'))
